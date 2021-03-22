f9a1193dca01bad6846fc25e1b0fae8c0b3fc601
PuyangChen <Niocholas_cpy@163.com>
2021-03-22 23:04:53 +0800

    version-2 0322

---
+++ src/js/project0.js
@@ -0,0 +1,24 @@
+/*-*- codeing = utf-8 -*-*/
+/*@Time : 2021-03-20 19:18*/
+/*@Author: Puyang Chen*/
+/*@File : project0.js*/
+/*@Software: WebStorm*/
+
+window.onload=selectOption;
+function selectOption(){
+    document.getElementById('answer1').value=document.getElementById('question1').value;
+    document.getElementById('answer2').value=document.getElementById('question2').value;
+    document.getElementById('answer3').value=document.getElementById('question3').value;
+    document.getElementById('answer4').value=document.getElementById('question4').value;
+    document.getElementById('answer5').value=document.getElementById('question5').value;
+    document.getElementById('answer6').value=document.getElementById('question6').value;
+}
+
+function lucky(){
+
+}
+
+document.getElementById("lucky").onclick = function() {
+    document.getElementById("btnl").value="I'm Feeling Lucky";
+    document.getElementById("identify").value="AINFCbYAAAAAYFipKRhsgcOAIvuHmxY-z3M7KRb0p8FJ";
+};
